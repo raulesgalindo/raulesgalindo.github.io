1527f92277661648ea7e9066f4deb7a9e6b43384
Raul Galindo <raulgalindorosales@gmail.com>
2022-08-27 17:42:40 -0600

    Updates

---
+++ precache-manifest.eb5049489bef3bc6920e5457aaa391dc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e63405c383270b08b9b3ff1feb9a89b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "219719f76e865e4ad2da",
+    "url": "/static/css/2.97a0bcc1.chunk.css"
+  },
+  {
+    "revision": "5820523dca9e5e502fd0",
+    "url": "/static/css/main.2a6d49af.chunk.css"
+  },
+  {
+    "revision": "219719f76e865e4ad2da",
+    "url": "/static/js/2.d9fae843.chunk.js"
+  },
+  {
+    "revision": "fa00fad1a9531fa50c3e1edb986db786",
+    "url": "/static/js/2.d9fae843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5820523dca9e5e502fd0",
+    "url": "/static/js/main.05028721.chunk.js"
+  },
+  {
+    "revision": "f72de0c40d0c6801007a",
+    "url": "/static/js/runtime-main.54153d0e.js"
+  },
+  {
+    "revision": "fe8bed5feea747e0797e24c4e8b26bb2",
+    "url": "/static/media/404.fe8bed5f.png"
+  },
+  {
+    "revision": "939d78a0842e638839c6216da3646d48",
+    "url": "/static/media/blog-hero.939d78a0.png"
+  },
+  {
+    "revision": "4a8c832994b48ee4bfc599baaae4410e",
+    "url": "/static/media/blog-single.4a8c8329.jpg"
+  },
+  {
+    "revision": "9c9a750812bab056b475fb9cc078f51e",
+    "url": "/static/media/favicon.9c9a7508.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "53da834cf982da90647dc32565f82abf",
+    "url": "/static/media/service-bg.53da834c.png"
+  },
+  {
+    "revision": "34c6010a2a87fcd287f2680335bc516a",
+    "url": "/static/media/slider-1.34c6010a.jpg"
+  }
+]);
